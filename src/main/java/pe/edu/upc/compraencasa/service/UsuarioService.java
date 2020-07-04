cc14065987d390e11886281901561c1723f27450
user.email <the.vendos_14@hotmail.com>
2020-07-04 10:53:02 -0500

    1stcommit

--- src/main/java/pe/edu/upc/compraencasa/service/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.compraencasa.service;
-
-import java.util.Optional;
-
-import pe.edu.upc.compraencasa.model.entity.Usuario;
-
-public interface UsuarioService extends CrudService<Usuario, Long> {
-	Optional<Usuario> findByUsername (String username) throws Exception;
-}
