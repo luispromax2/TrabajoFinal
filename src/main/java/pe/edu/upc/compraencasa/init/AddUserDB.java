cad9a0c3d5d4227c5137cd11c2c9304e41461f19
luispromax2 <lucho2571@hotmail.com>
2020-07-03 21:36:43 -0500

    compraencasa

---
+++ src/main/java/pe/edu/upc/compraencasa/init/AddUserDB.java
@@ -0,0 +1,70 @@
+package pe.edu.upc.compraencasa.init;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import pe.edu.upc.compraencasa.controller.AuthorityRepository;
+import pe.edu.upc.compraencasa.model.entity.Usuario;
+import pe.edu.upc.compraencasa.model.repository.UsuarioRepository;
+
+@Service
+public class AddUserDB implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private AuthorityRepository authorityRepository;
+	
+	/*@Autowired
+	private PasswordEncoder passwordEncoder;*/
+	
+	@Override
+	public void run(String... args) throws Exception {
+		/*Usuario comprador = new Usuario();
+		comprador.setUsername("comprador1");
+		comprador.setPassword(new BCryptPasswordEncoder().encode("comprador"));
+		comprador.setEnable(true);
+		
+		Usuario vendedor= new Usuario();
+		vendedor.setUsername("vendedor1");
+		vendedor.setPassword(new BCryptPasswordEncoder().encode("vendedor"));
+		vendedor.setEnable(true);
+		
+		Usuario admin= new Usuario();
+		admin.setUsername("admin1");
+		admin.setPassword(new BCryptPasswordEncoder().encode("admin"));
+		admin.setEnable(true);
+		
+		comprador.addAuthority("ROLE_BUYER");
+		comprador.addAuthority("ACCESS_ADDUSERCOMPRADOR");
+		comprador.addAuthority("ACCESS_ADDPRODUCTOACOMPRAR");
+		comprador.addAuthority("ACCESS_EDITPRODUCTOACOMPRAR");
+		comprador.addAuthority("ACCESS_DELPRODUCTOACOMPRAR");
+		comprador.addAuthority("ACCESS_COMPRARPRODUCTO");
+		
+		vendedor.addAuthority("ROLE_SELLER");
+		vendedor.addAuthority("ACCESS_ADDUSERVENDEDOR");
+		vendedor.addAuthority("ACCESS_ADDPRODUCTOAVENDER");
+		vendedor.addAuthority("ACCESS_EDITPRODUCTOAVENDER");
+		vendedor.addAuthority("ACCESS_DELPRODUCTOAVENDER");
+		
+		admin.addAuthority("ROLE_ADMIN");
+		admin.addAuthority("ACCESS_PRODUCTOAGREGADO");
+		admin.addAuthority("ACCESS_ADDUSERVENDEDOR");
+		admin.addAuthority("ACCESS_ADDUSERCOMPRADOR");
+		admin.addAuthority("ACCESS_EDITCOMPRADOR");
+		admin.addAuthority("ACCESS_DELCOMPRADOR");
+		admin.addAuthority("ACCESS_EDITVENDEDOR");
+		admin.addAuthority("ACCESS_DELVENDEDOR");
+		
+		usuarioRepository.save(comprador);
+		usuarioRepository.save(vendedor);
+		usuarioRepository.save(admin);*/
+		
+		
+	}
+
+}
---
+++ src/main/java/pe/edu/upc/compraencasa/model/entity/Factura.java
@@ -0,0 +1,42 @@
+package pe.edu.upc.compraencasa.model.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "facturas")
+@Getter
+@Setter
+public class Factura {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank(message ="No puede estar vacio")
+	@Column(name = "valor_total",nullable = false)
+	private double valorTotal;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+    private Compra compra;
+
+    @OneToOne(mappedBy="factura",fetch = FetchType.LAZY)
+    @JoinColumn(name="detalleFactura_id", updatable = false, nullable = false)
+    private DetalleFactura detalleFactura;
+}
